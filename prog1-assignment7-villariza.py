8e02f03096a922d7356eeea501e47bd098cf4206
Fritz Cedrick Villariza <villarizaced@gmail.com>
2021-11-27 21:42:45 +0800

    Arranged the Python Program such as: (Define Function, Renaming Variables, and using Special Functions [format])

--- prog1-assignment7-villariza.py
+++ prog1-assignment7-villariza.py
@@ -6,20 +6,25 @@
 # vowels: 6
 # consonants: 8
 
-sentence = input("\nEnter your SENTENCE in the space provided below.\n> ")
-strLoop = sentence.upper() # strLoop = String Loop, and used an .upper() function for an approachable if/elif code block statement.
-wordCount = {}
-numWord = 1 # Number of Words = numerical string is set to 1. (for every space after a word, there corresponds an increment in the variable data in numWord.)
-numVwl = 0 # Number of Vowels = numerical string is set to 0. (for each vowel character is satisfied through if/elif statement from for loop, there corresponds an increment in the variable data in numVwl.)
-numCons = 0 # Number of Consonant = numerical string is set to 0. (for each consonant character is satisfied through if/elif statement from for loop, there corresponds an increment in the variable data in numVwl.)
+senChar = input("\nEnter your SENTENCE in the space provided below.\n> ") # Sentence Character , and is used for input function.
 
-for index in strLoop:
-    if index in "AEIOU":
-        numVwl += 1
-    elif index in "BCDFGHJKLMNPQRSTVWXYZ":
-        numCons += 1
-    elif index in " ":
-        numWord += 1
+def convoke(numWord = 1, numVwl = 0, numCons = 0):
+    strLoop = senChar.upper()
+    """
+    String Loop, and used an .upper() function for an approachable if/elif code block statement.
+    """
+    for index in strLoop:
+        if index in " ":
+            numWord += 1
+        elif index in "AEIOU":
+            numVwl += 1
+        elif index in "BCDFGHJKLMNPQRSTVWXYZ":
+            numCons += 1
+    return numWord, numVwl, numCons
 
-print(f"\nYour Input \n{sentence} | is accounted on the Python program following a statistics in character sentence length.")
-print(f"\nWords: {numWord} \nVowels: {numVwl} \nConsonant: {numCons}\n")
+fnl_Word, fnl_Vwl, fnl_Cons = convoke()
+
+fnl_usrInput = '\nYour Input \n\"{}\" | is accounted on the Python program following a statistics in character sentence length.'.format(senChar)
+fnl_Statement = '\nWords: {} \nVowels: {} \nConsonant: {}\n'.format(fnl_Word, fnl_Vwl, fnl_Cons)
+print(fnl_usrInput)
+print(fnl_Statement)
